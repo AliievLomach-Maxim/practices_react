2674e98be04e5b5dbdcfdecf56e68620af5ed390
AliievLomachMaxim <anastasiia.chikrizova@yassir.com>
2023-05-21 22:14:26 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import UsersList from './components/UsersList'
+import users from './users.json'
+
+function App() {
+	return <UsersList users={users} />
+}
+
+export default App
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import User from './User'
+import PropTypes from 'prop-types'
+
+const UsersList = ({ users }) => {
+	return (
+		<ul>
+			{users.map((el) => (
+				<User key={el.id} user={el} />
+			))}
+		</ul>
+	)
+}
+
+UsersList.propTypes = {
+	users: PropTypes.arrayOf(PropTypes.object),
+}
+
+export default UsersList
