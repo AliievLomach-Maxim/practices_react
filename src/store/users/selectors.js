98e33f5bab103660ba76844887fd100f52749dc7
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-07-02 17:20:52 +0300

    add less

--- src/store/users/selectors.js
+++ src/store/users/selectors.js
@@ -1,1 +1,24 @@
-export const usersSelector = state => state.users.users
+import { createSelector } from '@reduxjs/toolkit'
+
+const usersItemsSelector = state => state.users.users
+export const filterSelector = state => state.users.filter
+
+export const usersSelector = createSelector(
+    [usersItemsSelector, filterSelector],
+    (users, filter) => {
+        if (!users.items) {
+            return users
+        }
+
+        const lowercaseFilter = filter.toLowerCase()
+        const filteredItems = users.items?.filter(
+            user =>
+                user.firstName.toLowerCase().includes(lowercaseFilter) ||
+                user.phone.replaceAll(' ', '').includes(filter)
+        )
+        return {
+            ...users,
+            items: filteredItems || null,
+        }
+    }
+)
